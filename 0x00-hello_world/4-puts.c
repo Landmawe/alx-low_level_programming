8e6839ff1f5f446990fb0ce23f24c249bc7ab78c
Landmawe <mureithieric401@gmail.com>
2023-02-22 01:48:50 -0800

    0-preprocessor

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * -main programming is like using a multi lingual puzzle
+ *  return 0 (well done)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
